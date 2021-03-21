7f30a75b5ec70e56c933d400acf6301c308213ed
huyp26102000 <huyp26102000@gmail.com>
2021-03-22 02:00:07 +0700

    update gradle.properties for fixing build error project

--- talkWithDoctor_Mobile/android/app/src/main/java/com/talkwithdoctor_mobile/MainActivity.java
+++ talkWithDoctor_Mobile/android/app/src/main/java/com/talkwithdoctor_mobile/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "talkWithDoctor_Mobile";
+    return "TalkWithDoctor_Mobile";
   }
 }
--- talkWithDoctor_Mobile/ios/talkWithDoctor_MobileTests/talkWithDoctor_MobileTests.m
+++ talkWithDoctor_Mobile/ios/talkWithDoctor_MobileTests/talkWithDoctor_MobileTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface talkWithDoctor_MobileTests : XCTestCase
+@interface TalkWithDoctor_MobileTests : XCTestCase
 
 @end
 
-@implementation talkWithDoctor_MobileTests
+@implementation TalkWithDoctor_MobileTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
